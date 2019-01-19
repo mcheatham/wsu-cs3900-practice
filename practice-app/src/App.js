89b37e45e33d1249a29b6941e0d171ee39205f43
Jared <jared.cole@outlook.com>
2019-01-19 15:00:04 -0500

    Created UI, Header.js, EncryptButton.js,

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Header1 from "./Header.js";
+import Encrypt from "./EncryptButton.js";
+
+
+
 
 class App extends Component {
   render() {
     return (
+
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <Header1/>
+
+        <Encrypt/>
+
+
       </div>
     );
-  }
-}
 
+  }
+
+
+
+
+}
 export default App;
